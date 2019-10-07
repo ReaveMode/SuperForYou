4e74aa16347991cbe8df6e5bda3bad8e0f2a3e3b
ReaveMode <sebbiejordan@hotmail.com>
2019-10-07 17:39:31 +0100

    test

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SuperForYou' });
+  res.render('index', { title: 'SuperForYou test' });
 });
 
 module.exports = router;
