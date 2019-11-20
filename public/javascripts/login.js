652f2dab45efcbb1f56262a53c2cff955600f8ce
ReaveMode <sebbiejordan@hotmail.com>
2019-11-20 17:20:32 +0000

    fixed mockup issue + added login modal form + fixed other small issues

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,7 +2,6 @@ function login() {
     var username = document.getElementById("uname").value
     var password = document.getElementById("psword").value
     if (username == "Sebby" && password == "Jordan") {
-        alert("You sucessfully logged in!")
         window.location = "MainGeneric.html"
     }
     else {
